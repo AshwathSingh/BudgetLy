926594ead25d8a23c826f7d0dbccf0bfb49e26a1
Ashwath <ashwath.singh2005@gmail.com>
2025-07-08 00:11:19 +0530

    Added a new category expenses view (still hard-coded) to serve as a better way to get an overview

---
+++ Views/ExpensesPerCategoryView.swift
@@ -0,0 +1,18 @@
+//
+//  ExpensesPerCategoryView.swift
+//  BudgetLy
+//
+//  Created by Ashwath Singh on 08/07/25.
+//
+
+import SwiftUI
+
+struct ExpensesPerCategoryView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ExpensesPerCategoryView()
+}
