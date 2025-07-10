89fab6b1eb75f78f0a8967ae32b0461ac74df548
Ashwath <ashwath.singh2005@gmail.com>
2025-07-10 18:01:47 +0530

    Changed the way trends are shown to make them more valuable for the user

---
+++ Views/Sheet Views/CardSheets.swift
@@ -0,0 +1,18 @@
+//
+//  CardSheets.swift
+//  BudgetLy
+//
+//  Created by Ashwath Singh on 10/07/25.
+//
+
+import SwiftUI
+
+struct CardSheets: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CardSheets()
+}
